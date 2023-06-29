c496335900d783c1be1c81313f63643e7b025f57
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2023-06-29 22:20:15 +0300

    email sender works

--- src/components/ContactFormTest.js
+++ src/components/ContactFormTest.js
@@ -1,71 +1,97 @@
-
 import React from "react";
 import emailjs from "emailjs-com";
-import { Form, Input, TextArea, Button } from "semantic-ui-react";
+import { Input, TextArea, Button } from "semantic-ui-react";
 import Swal from "sweetalert2";
 
-
-const SERVICE_ID = "**************";
+const SERVICE_ID = "service_9glw5nt";
 const TEMPLATE_ID = "template_y1eicm6";
-const USER_ID = "****************";
+const USER_ID = "QI17P74nLvCH9VLcp";
 
 const FormTest = () => {
-
-    const handleOnSubmit = (e) => {
-        e.preventDefault();
-        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
-          .then((result) => {
-            console.log(result.text);
-            Swal.fire({
-              icon: "success",
-              title: "Message Sent Successfully"
-            })
-          }, (error) => {
-            console.log(error.text);
-            Swal.fire({
-              icon: "error",
-              title: "Ooops, something went wrong",
-              text: error.text,
-            })
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+      .then(
+        (result) => {
+          console.log(result.text);
+          Swal.fire({
+            icon: "success",
+            title: "Message Sent Successfully",
           });
-        e.target.reset()
-      };
+        },
+        (error) => {
+          console.log(error.text);
+          Swal.fire({
+            icon: "error",
+            title: "Ooops, something went wrong",
+            text: error.text,
+          });
+        }
+      );
+    e.target.reset();
+  };
 
   return (
-    <div className="FormTest">
-      <Form onSubmit={handleOnSubmit}>
-        <Form.Field
-          id='form-input-control-email'
-          control={Input}
-          label='Email'
-          name='user_email'
-          placeholder='Email…'
-          required
-          icon='mail'
-          iconPosition='left'
-        />
-        <Form.Field
-          id='form-input-control-last-name'
-          control={Input}
-          label='Name'
-          name='user_name'
-          placeholder='Name…'
-          required
-          icon='user circle'
-          iconPosition='left'
-        />
-        <Form.Field
-          id='form-textarea-control-opinion'
-          control={TextArea}
-          label='Message'
-          name='user_message'
-          placeholder='Message…'
-          required
-        />
-        <Button type='submit' color='green'>Submit</Button>
-      </Form>
-    </div>
+    <section id="contact" className="section has-background-primary-light pb-4">
+      <div className="container">
+        <div className="columns is-centered">
+          <div className="column is-half">
+            <h2 className="title is-2 has-text-centered has-text-black">
+              Ota yhteyttä
+            </h2>
+            <form onSubmit={handleOnSubmit} className="has-shadow">
+              <div className="field">
+                <label className="label has-text-black">Nimi</label>
+                <div className="control">
+                  <Input
+                    fluid
+                    type="text"
+                    name="user_name"
+                    placeholder="Nimesi"
+                    required
+                    icon="user circle"
+                    iconPosition="left"
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label has-text-black">Sähköposti</label>
+                <div className="control">
+                  <Input
+                    fluid
+                    type="email"
+                    name="user_email"
+                    placeholder="Sähköpostiosoitteesi"
+                    required
+                    icon="mail"
+                    iconPosition="left"
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label has-text-black">Viesti</label>
+                <div className="control">
+                  <TextArea
+                    placeholder="Viestisi"
+                    name="user_message"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <div className="control">
+                  <Button type="submit" color="green" fluid>
+                    Lähetä
+                  </Button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
 
 export default FormTest;

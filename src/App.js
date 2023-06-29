b7e79d5a9c6743a9b95bd4648bd8749c62db7c12
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2023-06-29 11:37:42 +0300

    fixinf

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import 'bulma/css/bulma.css';
 import './App.css';
 import Homepage from './pages/Homepage';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Pricing from './pages/Pricing';
 
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Navbar />
+      <Routes>
+        <Route path='/Homepage' exact component={Homepage} />
+        <Route path='/Pricing' component={Pricing} />
+      </Routes>
+    </Router>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,52 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 
-// Import your pages/components here
+import {
+    Nav,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink,
+  } from './NavbarElements';
 
 
 function MyComponent() {
 
-    const [isActive, setisActive] = React.useState(false)
+
     
-      return (
-        <nav className='navbar' role='navigation' aria-label='main navigation'>
-          <div className='navbar-brand'>
-            <a href='/' className='navbar-item'>
-              <img
-                src='https://bulma.io/images/bulma-logo.png'
-                alt='Logo'
-                width='112'
-                height='28'
-              />
-            </a>
-    
-            <a
-              role='button'
-              className={'navbar-burger burger'}
-              aria-label='menu'
-              aria-expanded='false'
-              data-target='navbarBasicExample'
-            >
-              <span aria-hidden='true'></span>
-              <span aria-hidden='true'></span>
-              <span aria-hidden='true'></span>
-            </a>
-          </div>
-          <div id='navbarBasicExample' className='navbar-menu'>
-            <div className='navbar-end'>
-              <div className='navbar-item'>
-                <a href='/' className='navbar-item'>
-                  Home
-                </a>
-                <a href='/' className='navbar-item'>
-                  Documentation
-                </a>
-              </div>
-            </div>
-          </div>
-        </nav>
-      )
-    }
+    return (
+        <>
+          <Nav>
+            <Bars />
+      
+            <NavMenu>
+              <NavLink to='./Homepage' activeStyle>
+                Home
+              </NavLink>
+              <NavLink to='/Pricing' activeStyle>
+                Pricing
+              </NavLink>
+             
+            </NavMenu>
+            <NavBtn>
+              <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+            </NavBtn>
+          </Nav>
+        </>
+      );
+    };
 
 
 export default MyComponent;
